9582f3106c69e688572c29f7538ed8c54c55ee75
ta-watanabe <ta-watanabe@link-tech.jp>
2023-10-25 15:16:01 +0900

    DBの接続確認について,エラーの詳細を出力するように修正

--- app/pages/test/db_connection_test.tsx
+++ app/pages/test/db_connection_test.tsx
@@ -1,25 +1,53 @@
 // server/pages/db.tsx
 
-import { table } from 'console';
+import { customLog } from '@/utils/customLog';
 import * as mysql from 'promise-mysql';
 
 // MySQLデータベースに接続してデータを取得する関数
 async function fetchData() {
-  const connection = await mysql.createConnection({
-    // 接続テスト用,本来ここに直接パスワードを書いたりはダメ
-    host: 'db', // どこからとってくるのかは不明,コンテナサービス名らしいが...？
-    user: 'root',
-    port: 3306,
-    password: 'password',
-    database: 'sampleDB'
-  });
+  let connection: mysql.Connection | undefined;
+  try {
+    connection = await mysql.createConnection({
+      // 接続テスト用,本来ここに直接パスワードを書いたりはダメ
+      host: 'db', // どこからとってくるのかは不明,コンテナサービス名らしいが...？
+      user: 'root',
+      port: 3306,
+      password: 'password',
+      database: 'sampleDB'
+    });
+    console.log('Connection successful');
+  } catch (error: any) {
+    switch (error.code) {
+      case 'ENOTFOUND':
+      case 'EHOSTUNREACH':
+        console.error('Host does not exist or is unreachable');
+        break;
+      case 'ECONNREFUSED':
+        console.error('Port is not open on the host');
+        break;
+      case 'ER_ACCESS_DENIED_ERROR':
+        console.error('Invalid user or password');
+        break;
+      case 'ER_NOT_SUPPORTED_AUTH_MODE':
+        console.error('root user cannot use');
+        break;
+      case 'ER_BAD_DB_ERROR':
+        console.error('Database does not exist');
+        break;
+      default:
+        console.error('Unknown error: ', error);
+    }
+    customLog(error.sqlMessage);
+    if (connection && connection.end)await connection.end();
+    return null;
+  }
 
   const data = await connection.query('SELECT * FROM sampleTable');
   const serializableData = data.map((item: { dateColumn: { toISOString: () => any; }; }) => ({
     ...item,
     dateColumn: item.dateColumn ? item.dateColumn.toISOString() : null  // Dateオブジェクトを文字列に変換
   }));
-  await connection.end();
+  if (connection && connection.end)await connection.end();
   console.log(serializableData);
   return serializableData;
 }
